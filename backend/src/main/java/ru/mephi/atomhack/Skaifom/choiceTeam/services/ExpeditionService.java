282924b8ec2756abbd4978ab052bfccfe7b850a3
Petrov <74093293+RedArmyIsStrongestAll@users.noreply.github.com>
2025-03-20 05:08:06 +0300

    - интерфейс ExpeditionService

---
+++ backend/src/main/java/ru/mephi/atomhack/Skaifom/choiceTeam/services/ExpeditionService.java
@@ -0,0 +1,30 @@
+package ru.mephi.atomhack.Skaifom.choiceTeam.services;
+
+import ru.mephi.atomhack.Skaifom.choiceTeam.entity.*;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ExpeditionService {
+    Optional<ExpeditionDTO> addExpedition(String name, String description);
+
+    List<Integer> getAllExpeditionIds();
+
+    List<HeroDTO> getHeroesByExpedition(int expeditionId);
+
+    List<TaskDTO> getExpeditionTasks(int expeditionId);
+
+    List<SubTaskDTO> getSubtasks(int taskId);
+
+    List<BacklogDTO> getBacklog();
+
+    Optional<SubTaskDTO> addSubtask(int taskId, int backlogId);
+
+    Optional<TaskDTO> addTaskToExpedition(int expeditionId, String name, String description);
+
+    boolean deleteSubtask(int taskId, int subTaskId);
+
+    boolean deleteTask(int taskId);
+
+    boolean deleteExpedition(int expeditionId);
+}
